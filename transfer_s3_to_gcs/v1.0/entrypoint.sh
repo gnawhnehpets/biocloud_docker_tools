b8472076e06bd6abff34ebd9c879c41486776051
gnawhnehpets <stephenyounghwang@gmail.com>
2024-07-17 15:40:14 -0400

    added transfer job status

--- transfer_s3_to_gcs/v1.0/entrypoint.sh
+++ transfer_s3_to_gcs/v1.0/entrypoint.sh
@@ -5,19 +5,17 @@
 if [ -n "$GC_ADC_FILE" ]; then
     echo "[[ Activating Google Cloud service account ]]"
     gcloud auth activate-service-account --key-file="$GC_ADC_FILE" --no-user-output-enabled
-    # Add the code to handle the case when the variables are set
 elif [ -n "$GC_ADC_JSON" ]; then
     echo "--- GC_ADC_JSON detected ---"
     echo "[[ Create Google Service Account key file ]]"
     echo $GC_ADC_JSON > /opt/adc.json
     echo "[[ Activating Google Cloud service account ]]"
     gcloud auth activate-service-account --key-file="/opt/adc.json" --no-user-output-enabled
-
-    # Add the code to handle the case when the variables are not set
 else
     echo "GC_ADC_FILE or GC_ADC_JSON not detected."
     exit 1
 fi
+
 echo "[[ Create temporary AWScreds.txt file ]]"
 echo "{ \"accessKeyId\": \"$AWS_ACCESS_KEY_ID\", \"secretAccessKey\": \"$AWS_SECRET_ACCESS_KEY\" }" > /opt/AWScreds.txt
 
@@ -25,18 +23,18 @@ echo "{ \"accessKeyId\": \"$AWS_ACCESS_KEY_ID\", \"secretAccessKey\": \"$AWS_SEC
 if [[ " $* " =~ " --run " ]]; then
     echo "--run flag is present."
     echo "[[ Run transfer job { $S3_BUCKET } ]]"
-    gcloud transfer jobs run "$S3_BUCKET" --project "$GC_PROJECT"
+    TRANSFER_JOB_ID=$(gcloud transfer jobs run "$S3_BUCKET" --project "$GC_PROJECT" --format="value(name)")
 else
-    # echo "--run flag is not present."
     echo "[[ Initiate transfer from s3:// to gs://$S3_BUCKET ]]"
-    gcloud transfer jobs create s3://"$S3_BUCKET" gs://"$S3_BUCKET" \
+    TRANSFER_JOB_ID=$(gcloud transfer jobs create s3://"$S3_BUCKET" gs://"$S3_BUCKET" \
     --name "$S3_BUCKET" \
     --description "$S3_BUCKET" \
     --source-creds-file /opt/AWScreds.txt \
     --project "$GC_PROJECT" \
     --overwrite-when 'different' \
     --delete-from 'destination-if-unique' \
-    --no-enable-posix-transfer-logs
+    --no-enable-posix-transfer-logs \
+    --format="value(name)")
 fi
 
 echo "[[ Cleanup AWScreds.txt file ]]"
@@ -47,3 +45,19 @@ if [ -n "$GC_ADC_JSON" ]; then
     echo "[[ Cleanup GAC key file ]]"
     rm /opt/adc.json
 fi
+
+# Check the status of the transfer job until it completes or fails
+while true; do
+    STATUS=$(gcloud transfer jobs describe "$TRANSFER_JOB_ID" --format="value(latestOperation.status)")
+
+    if [ "$STATUS" == "SUCCESS" ]; then
+        echo "Transfer job completed successfully."
+        exit 0
+    elif [ "$STATUS" == "FAILED" ]; then
+        echo "Transfer job failed."
+        exit 1
+    else
+        echo "Transfer job status: $STATUS. Checking again in 60 seconds..."
+        sleep 60
+    fi
+done
